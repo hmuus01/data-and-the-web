0abe3e96e3479879690e8c153fd0585d3fb2d956
Hamze Muuse <hmuus001@gold.ac.uk>
2019-01-29 10:51:25 +0000

    Lab 12 part 2

--- lab-12/jinja_hello.py
+++ lab-12/jinja_hello.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/hello/')
-@app.route('/hello/<name>')
+@app.route('/welcome/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name)
 
