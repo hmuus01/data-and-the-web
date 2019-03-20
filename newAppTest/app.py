3f7511f12848108e799bdbf4e5e8501e12fe4cfc
Hamze Muuse <hmuus001@gold.ac.uk>
2019-03-20 11:32:23 +0000

    add and edit article error found and fixed

--- newAppTest/app.py
+++ newAppTest/app.py
@@ -224,8 +224,8 @@ def dashboard():
 
 # Article Form Class
 class ArticleForm(Form):
-    title = StringField('Title', [validators.Length(min=1, max=200)])
-    body = TextAreaField('Body', [validators.Length(min=30)])
+    title = StringField('Title', validators = [validators.DataRequired()])
+    body = TextAreaField('Body', validators =  [validators.DataRequired()])
 
 # Add Article
 @app.route('/add_article', methods=['GET', 'POST'])
